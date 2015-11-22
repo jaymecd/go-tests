012ba2a7720ffdc3366e2b80f4b31f47bf5dd383
Nikolai Zujev <nikolai.zujev@gmail.com>
2015-11-22 22:55:46 +0100

    fix

--- myapp.go
+++ myapp.go
@@ -3,13 +3,13 @@ package main
 import "fmt"
 
 var (
-	Version = "undefined"
+	Version   = "undefined"
 	BuildTime = "undefined"
-    GitHash = "undefined"
+	GitHash   = "undefined"
 )
 
 func main() {
-    fmt.Printf("Version    : %s\n", Version)
-    fmt.Printf("Git Hash   : %s\n", GitHash)
+	fmt.Printf("Version    : %s\n", Version)
+	fmt.Printf("Git Hash   : %s\n", GitHash)
 	fmt.Printf("Build Time : %s\n", BuildTime)
 }
